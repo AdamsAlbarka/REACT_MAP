5e8989f8d73072ae3749251d6e1f56bdbe14c5b9
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-08-09 23:00:22 +0100

    Update App.js
    Upadated APPJS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
 import * as React from 'react';
 import Map, {Marker, Popup} from 'react-map-gl';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import StarIcon from '@mui/icons-material/Star';
 import "./app.css"
-
+import axios from "axios"
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
-  const [lat, setLat] = useState(37.8)
-  const [long, setLong] = useState(-122.4)
-  const [showPopup, setShowPopup] = React.useState(true);
+  const [pins, setPins] = useState([])
+
+useEffect(() => {
+  const getPins = async () => {
+    try {
+      const res = await axios.get("/pins");
+      setPins(res.data)
+    } catch (err) {
+      console.log(err);
+    }
+  } 
+  getPins()
+}, [])
+
 
   return (
     <div className='App'>
       <Map
         initialViewState={{
-          latitude:lat,
-          longitude:long,
+          latitude:-200,
+          longitude:-120,
           zoom: 4
         }}
         style={{
@@ -28,41 +39,41 @@ function App() {
 
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         >
-
+      {pins.map(p => (
+        <>
+        
         <Marker 
-        longitude={long} 
-        latitude={lat} 
+        longitude={p.long} 
+        latitude={p.lat} 
         color="red" 
         offsetLeft={-20}
         offsetTop={-10}
         />
 
-      {showPopup && (
       <Popup 
-        longitude={long} 
-        latitude={lat}
+        longitude={p.long} 
+        latitude={p.lat}
         anchor="left"
-        // onClose={() => setShowPopup(false)}
         >
           <div className='card'>
             <label>Place</label>
-            <h4>Eiffel Tower</h4>
+            <h4>{p.title}</h4>
             <label>Review</label>
-            <p className='desc'>Beautiful place to be</p>
+            <p className='desc'>{p.desc}</p>
             <label>Rating</label>
             <div className='stars'>
 
-              <StarIcon  className='star' />
               <StarIcon  className='star'/>
               <StarIcon  className='star'/>
-              <StarIcon className='star' />
+              <StarIcon  className='star'/>
+              <StarIcon  className='star'/>
               <StarIcon  className='star'/>
 
             </div>
             <label>Information</label>
              <span className='username'>
               Created by
-              <b>Adlers</b>
+              <b>{p.username}</b>
              </span>
 
              <span className='date'>
@@ -70,7 +81,10 @@ function App() {
              </span>
 
           </div>
-      </Popup>)}
+      </Popup>
+          </>
+        ))}
+      
       </Map>
     
     </div>
@@ -78,21 +92,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-// const [viewport, setViewport] = useState({
-//   width: "100vw",
-//   height: "100vh",
-//   latitude: 46,
-//   longitute: 17,
-//   zoom:4
-// })
-
-// return (
-//   <div className='App'>
-//     <ReactMapGL>
-//       {...viewport}
-//       map
-//     </ReactMapGL>
