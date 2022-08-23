7a56ff0140d6ef5209a4ba6a49d2a05503877fe9
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-08-23 05:43:45 +0100

    Good State
    This code is now in good state. All errors fixed at exactly 4:40am.  Now progressing to the next phase of development.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,27 @@
 import * as React from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import { useState, useEffect } from 'react';
-import {Star, Room } from '@mui/icons-material';
+import {Star } from '@mui/icons-material';
+import RoomIcon from '@mui/icons-material/Room';
 import "./app.css"
 import axios from "axios"
-import { format } from "timeago.js"
-
 import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+import * as timeago from 'timeago.js';
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRsZXJzIiwiYSI6ImNsNzNvdGY4dDBndGM0MG53dzBlNnk4cDcifQ.vZ78yh-2-_kgd0vv6d5eXw';
 
 function App() {
   const [pins, setPins] = useState([])
-  const [currentUsername, setCurrentUsername] = useState('Adlers')
-  const [currentPlaceId, setCurrentPlaceId] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 47.040182,
-    longitude: 17.071727,
-    zoom: 4,
-  });
+  const [currentPlace, setCurrentPlace] = useState([])
+  const currentUser = "Adlers"
 
 
   useEffect(() => {
     const getPins = async () => {
       try {
-        const allPins = await axios.get("/pins");
-        setPins(allPins.data); 
+        const res = await axios.get("/pins");
+        setPins(res.data); 
       } catch (error) {
         console.log(error.response.data);
       }
@@ -31,53 +29,62 @@ function App() {
     getPins();
   }, []);
 
+  const handleMarkerClick = (id) => {
+    setCurrentPlace(id)
+  }
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-      width="100%"
-      height="100%"
-      transitionDuration="200"
-      mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
-      onViewportChange={(viewport) => setViewport(viewport)}
-      // onDblClick={currentUsername && handleAddClick}
+    <Map
+     initialViewState={{
+      latitude: 47.040182,
+      longitude: 17.071727,
+      zoom: 4,
+    }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      // onViewportChange={(viewport) => setViewport(viewport)}
     >
-      {pins.map((p) => (
+
+      {pins.map(p => (
         <>
-      <Marker longitude={-100} latitude={40} anchor="bottom" >
-        {/* <img src="./pin.png" /> */}
-      </Marker>
-          {p._id === currentPlaceId && (
-            <Popup
-              key={p._id}
-              latitude={p.lat}
-              longitude={p.long}
-              closeButton={true}
-              closeOnClick={false}
-              onClose={() => setCurrentPlaceId(null)}
-              anchor="left"
-            >
-              <div className="card">
-                <label>Place</label>
-                <h4 className="place">{p.title}</h4>
-                <label>Review</label>
-                <p className="desc">{p.desc}</p>
-                <label>Rating</label>
-                <div className="stars">
-                  {Array(p.rating).fill(<Star className="star" />)}
-                </div>
-                <label>Information</label>
-                <span className="username">
-                  Created by <b>{p.username}</b>
-                </span>
-                <span className="date">{format(p.createdAt)}</span>
-              </div>
-            </Popup>
-          )}
-        </>
-      ))}
-      </ReactMapGL>
+       <Marker longitude={p.long} latitude={p.lat} 
+       offsetLeft={-20}
+       offsetTop={-10}
+       anchor="bottom" >
+       <RoomIcon className='roomIcon' 
+                 style={{color:p.username === currentUser ? "tomato" : "slateblue"}} 
+                 onClick={() => handleMarkerClick(p._id)} /> 
+       </Marker>
+       
+        {p._id === currentPlace && (
+       <Popup 
+       longitude={p.long} 
+       latitude={p.lat}
+       anchor="left"
+       onClose={() => setCurrentPlace(null)}
+       >
+          <div className='card'>
+            <label >Place</label>
+            <h4 className='place'>{p.title}</h4>
+            <label>Review</label>
+            <p>{p.desc}</p>
+            <label>Rating</label>
+            <div className='stars'>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+            </div>
+            <label>Information</label>
+            <span className='username'>created by <b>{p.username}</b></span>
+            <span className='date'>{timeago.format(p.createdAt)}</span>
+          </div>
+      </Popup> 
+            )}
+    </>
+    ))}
+      </Map>
 </div>
   );
 }
