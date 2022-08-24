37a57399eaa940de6f842f121bd5e364848440bf
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-08-24 01:22:53 +0100

    Updated App
    Updated App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,15 +13,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWRsZXJzIiwiYSI6ImNsNzNvdGY4dDBndGM0MG53dzBlN
 
 function App() {
   const [pins, setPins] = useState([])
+  const [title, setTitle] = useState(null)
+  const [desc, setDesc] = useState(null)
+  const [rating, setRating] = useState(0)
   const [currentPlace, setCurrentPlace] = useState(null)
   const currentUser = "Adlers"
   const [newPlace, setNewPlace] = useState(null)
-  const [initialViewState, setInitialViewState] = useState({
-      latitude: 47,
-      longitude: 17,
-      zoom: 4,
-  })
-
+  const [viewport, setViewport] = useState();
 
   useEffect(() => {
     const getPins = async () => {
@@ -45,14 +43,41 @@ function App() {
       long, 
       lat
     }) 
-    // console.log(e)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long
+    }
+
+    try{
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null)
+    }
+    catch (err){
+      console.log(err)
+    }
+
   }
 
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+
     <Map
-     {...initialViewState}
-     mapStyle="mapbox://styles/mapbox/streets-v11"
+    initialViewState={{
+      latitude: 47.040182,
+      longitude: 17.071727,
+      zoom: 4,
+    }}
+    style={{width: "100vw", height: "100vh"}}
+     mapStyle="mapbox://styles/mapbox/streets-v9"
      onDblClick={handleAddClick}
      transitionDuration="200"
     >
@@ -63,11 +88,13 @@ function App() {
        <Marker 
             longitude={p.long} 
             latitude={p.lat} 
-            offsetLeft={-20}
-            offsetTop={-10}
+            offsetLeft={-viewport.zoom * 3.5}
+            offsetTop={-viewport.zoom * 7}
             anchor="bottom" >
        <RoomIcon className='roomIcon' 
-                 style={{color:p.username === currentUser ? "tomato" : "slateblue"}} 
+                 style={{
+                  fontSize: viewport.zoom * 7,
+                  color:p.username === currentUser ? "tomato" : "slateblue"}} 
                  onClick={() => handleMarkerClick(p._id)} /> 
        </Marker>
 
@@ -85,11 +112,7 @@ function App() {
             <p>{p.desc}</p>
             <label>Rating</label>
             <div className='stars'>
-              <Star className='star'/>
-              <Star className='star'/>
-              <Star className='star'/>
-              <Star className='star'/>
-              <Star className='star'/>
+              {Array(p.rating).fill(<Star className='star'/>)}      
             </div>
             <label>Information</label>
             <span className='username'>created by <b>{p.username}</b></span>
@@ -107,11 +130,40 @@ function App() {
       anchor="left"
       onClose={() => setCurrentPlace(null)}
       >
-  Hello World    
+   <div>
+                <form 
+                onSubmit={handleSubmit}
+                >
+                  <label>Title</label>
+                  <input
+                    placeholder="Enter a title"
+                    autoFocus
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Say us something about this place."
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                  <label>Rating</label>
+                  <select 
+                  onChange={(e) => setRating(e.target.value)}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <button type="submit" className="submitButton">
+                    Add Pin
+                  </button>
+                </form>
+              </div>    
   </Popup>
 )}
       </Map>
-</div>
+
   );
 }
 
